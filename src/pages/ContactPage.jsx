bc273067b89b61f9165588c0d456e97050542dc6
Pratik-z005 <kamble.pratiklive@gmail.com>
2026-03-12 18:15:56 +0530

    Add new feature

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -11,42 +11,59 @@ const ContactPage = () => {
   return (
     <>
       <Navbar />
-      <div className={css.container}>
+      <form className={css.container}>
         <div className={css.content}>
           <p>
             Contact If you have any questions or requests involving my work,
             feel free to contact me through any of my SNS or using the form
             below.
           </p>
-
-          <label>Full Name</label>
-          <input type="text" />
-
-          <br />
-
-          <label>Full Name</label>
-          <input type="text" />
-
-          <br />
-
-          <label>Full Name</label>
-          <input type="text" />
-
-          <br />
-
-          <label>Full Name</label>
-          <input type="text" />
-
-          <br />
-
-          <label>Full Name</label>
-          <textarea name="" id=""></textarea>
         </div>
+
+        <label>Full Name*</label>
+        <input
+          className={css.input}
+          type="text"
+          placeholder="Full Name"
+          required
+        />
 
         <br />
 
-        <input type="submit" />
-      </div>
+        <label>Organization</label>
+        <input className={css.input} type="text" placeholder="Organization" />
+
+        <br />
+
+        <label>Email*</label>
+        <input
+          className={css.input}
+          type="email"
+          placeholder="Email"
+          required
+        />
+
+        <br />
+
+        <label>Phone number</label>
+        <input className={css.input} type="number" placeholder="Phone number" />
+
+        <br />
+
+        <label>Message*</label>
+        <textarea
+          className={css.input}
+          name=""
+          id=""
+          placeholder="Message"
+          rows={5}
+          required
+        ></textarea>
+
+        <br />
+
+        <input className={css.input} id={css.submit} type="submit" />
+      </form>
 
       <Footer />
     </>
